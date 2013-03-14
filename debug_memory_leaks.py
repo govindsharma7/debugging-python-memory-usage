fc0c8ce9ba621cb86b6abb93adf1b297a7c0230b
Eliot <saltycrane@gmail.com>
2013-03-13 18:45:41 -0700

    Revert "Now running 2X gives expected results"
    This reverts commit b9e7de6dddf1785dc1010a76377b7a5f365ab36c.

--- debug_memory_leaks.py
+++ debug_memory_leaks.py
@@ -20,18 +20,6 @@ def main():
 
     gc.collect()
 
-    mylist = []
-    for x in xrange(100):
-        mystr = ' ' * int(size_kb) * 1024
-        mydict = {'mykey': mystr}
-        mylist.append(mydict)
-
-    del mystr
-    del mydict
-    del mylist
-
-    gc.collect()
-
     time.sleep(3600)
 
 
