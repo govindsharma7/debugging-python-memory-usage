b9e7de6dddf1785dc1010a76377b7a5f365ab36c
Eliot <saltycrane@gmail.com>
2013-03-13 18:43:40 -0700

    Now running 2X gives expected results

--- debug_memory_leaks.py
+++ debug_memory_leaks.py
@@ -20,6 +20,18 @@ def main():
 
     gc.collect()
 
+    mylist = []
+    for x in xrange(100):
+        mystr = ' ' * int(size_kb) * 1024
+        mydict = {'mykey': mystr}
+        mylist.append(mydict)
+
+    del mystr
+    del mydict
+    del mylist
+
+    gc.collect()
+
     time.sleep(3600)
 
 
