97d93cd165a139a6b6f96720de63a92561dd2f05
Eliot <saltycrane@gmail.com>
2013-03-19 18:38:16 -0700

    Reason for dropoff at 128KB uncovered
    Thanks to schlenk on Stack Overflow, found that the environment
    variable, MALLOC_MMAP_THRESHOLD_, controls the dropoff point.

--- runtest.sh
+++ runtest.sh
@@ -2,13 +2,22 @@
 
 output_file="output_$1.txt"
 
-echo "RSS S TTY TIME COMMAND" > $output_file
+echo "RSS COMMAND" > $output_file
 
-for i in `seq 1 1 200`;
+for j in $(seq 0 16384 262144);
 do
-    python $1 $i 2>&1 &
-    pid=$!
-    sleep 0.2
-    ps -e -O rss | grep $pid | grep python | awk '{$1 =""; print }' >> $output_file
-    kill $pid
+    in_kb=$(expr $j / 1024)
+    echo >> $output_file
+    echo "==================================================" >> $output_file
+    echo "MALLOC_MMAP_THRESHOLD_=$j ($in_kb KB)" >> $output_file
+    echo "==================================================" >> $output_file
+
+    for i in $(seq 1 1 260);
+    do
+        MALLOC_MMAP_THRESHOLD_=$j python $1 $i 2>&1 &
+        pid=$!
+        sleep 0.2
+        ps -o pid,rss,cmd | grep $pid | grep python | awk '{$1=""; print}' >> $output_file
+        kill $pid
+    done
 done
